cde231fbe6bb7eeefe58ab631d998728ad80e31c
Marwan Nakhaleh <mnakhaleh@gmail.com>
2016-04-07 18:23:09 -0400

    AHHHHHHgit add .git add .1!

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -5,34 +5,29 @@ class Mentor < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   def self.connect_to_linkedin(auth, signed_in_resource=nil)
-  	user = Mentor.where(provider: auth.provider, uid: auth.uid).first_or_create
-  	user = Mentor.create(name:auth.info.first_name,
+  	user = Mentor.where(provider: auth.provider, uid: auth.uid).first
+  	
+  	if user
+		user = Mentor.update(name:auth.info.first_name,
   							provider:auth.provider,
   							uid:auth.uid,
   							email:auth.info.email,
   							password:Devise.friendly_token[0,20],
   							)
-  	user.save!
-  	redirect_to root_url
+  		return user
+  	else
+  		registered_user = Mentor.where(:email => auth.info.email).first_or_create
+  		if registered_user
+  			return registered_user
+  		else
+  			user = Mentor.create(name:auth.info.first_name,
+  							provider:auth.provider,
+  							uid:auth.uid,
+  							email:auth.info.email,
+  							password:Devise.friendly_token[0,20],
+  							)
+  			redirect_to root_url
+  		end
+  	end
   end
-
-#  	if user
-#  		user.save!
-#  		return user
-#  	else
-#  		registered_user = Mentor.where(:email => auth.info.email).first_or_create
-#  		if registered_user
-#  			return registered_user
-#  		else
-#  			user = Mentor.create(name:auth.info.first_name,
-#  							provider:auth.provider,
-#  							uid:auth.uid,
-#  							email:auth.info.email,
-#  							password:Devise.friendly_token[0,20],
-#  							)
-#  			user.save!
-#  			redirect_to root_url
-#  		end
-#  	end
-#  end
 end
