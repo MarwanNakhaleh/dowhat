3c599a85103ff4bade0d2c5edc9c75742699cab2
Marwan Nakhaleh <mnakhaleh@gmail.com>
2016-04-07 17:47:04 -0400

    auth hash stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,7 @@
 class SessionsController < ApplicationController
 	def create
-		@mentor = Mentor.find_or_create_from_auth_hash(auth_hash)
+		@mentor = Mentor.find_or_create_from_auth_hash(request.env["omniauth.auth"])
 		self.current_mentor = @mentor
 		redirect_to root_url
-	end
-
-	protected
-
-	def auth_hash
-		request.env["omniauth.auth"]
 	end
 end		
