9f80eb4582c428c0ebdef29c62f3841d8b275196
Marwan Nakhaleh <mnakhaleh@gmail.com>
2016-04-07 16:25:57 -0400

    I wanna redirect to the very sparse dashboard!

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,11 +4,11 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 		@mentor = Mentor.connect_to_linkedin(request.env["omniauth.auth"],current_mentor)
 		if @mentor.persisted?
 			flash[:notice] = I18n.t "devise.omniauth_callbacks.success"
-			sign_in_and_redirect @mentor, :event => :authentication
+			sign_in @mentor, :event => :authentication
 			redirect_to root_url
 		else
 			session["devise.linkedin_uid"] = request.env["omniauth.auth"]
-			redirect_to root_url
+			redirect_to new_mentor_registration_url
 		end
 	end
 end
